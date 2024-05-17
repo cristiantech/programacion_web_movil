930616f2bbc68781a290ac9b5a5a69ab0d787a80
crisltech linux home <cristiand.munozt@outlook.com>
2024-05-16 21:02:30 -0500

    craete folder and files of week two

---
+++ weektwo/index.js
@@ -0,0 +1,51 @@
+
+// // let isBoolean = 21 % 2 === 0 ? "correcto" : "incorrecto";
+
+
+// // const myfruts = ["banana", "apple", "fruit", "peer"]
+
+// // // for( let i = 0; i < myfruts.length; i ++){
+// // //     console.log(`mi fruta favorita es ${myfruts[i]} y es deliciosa`)
+// // // }
+
+// // // const new_fruits = myfruts.map((i) => i.replace(/a/g, "e") )
+// // // myfruts
+// // // new_fruits
+// // // myfruts
+// // // new_fruits
+
+
+// // // funciones
+
+
+
+// // function get_hello(name){
+// //     console.log(`Bienevenido ${name}`)
+// // }
+
+// // // tipo flech
+
+// // const get_hello2 = (name) => `bienvenido ${name}`
+
+
+// // console.log(get_hello2("Alexis"))
+
+// // console.log(get_hello("renny"))
+
+
+
+
+
+
+
+// API que van utlizar 
+// Figma y su bosquejo
+// Asyncronismo
+// DOM
+
+
+
+
+
+
+
